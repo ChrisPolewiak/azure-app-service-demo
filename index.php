d20c73e5abf20f7fad21674595ba9afd13cf2515
sedael <chris@polewiak.pl>
2019-10-07 13:50:05 +0200

    fix

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
   </head>
 
   <body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-red border-bottom shadow-sm">
-      <h5 class="my-0 mr-md-auto font-weight-normal">Azure - <?php echo $_ENV["APPSETTING_AppName"] ?></h5>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <h5 class="my-0 mr-md-auto font-weight-normal text-primary">Azure - <?php echo $_ENV["APPSETTING_AppName"] ?></h5>
       <nav class="my-2 my-md-0 mr-md-3">
         <a class="p-2 text-dark" href="#">Features</a>
         <a class="p-2 text-dark" href="#">Enterprise</a>
@@ -62,7 +62,7 @@ if( isset($_SERVER["HTTP_X_MS_CLIENT_PRINCIPAL_ID"]) && $_SERVER["HTTP_X_MS_CLIE
       </div>
     </div>
 
-    <footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <footer class="pt-4 my-md-5 pt-md-5 border-top fixed-bottom">
       <div class="row">
         <div class="col-6 col-md">
           <h5>Learning</h5>
